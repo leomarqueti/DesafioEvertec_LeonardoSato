ae504891bebb966f1bd85e03f9c0d577d1e88162
Leonardo Sato <domme@DESKTOP-BDV1AKL>
2026-01-20 12:27:52 -0300

    Adicionado o `Swagger` ao projeto

--- backend/PontosTuristicos/PontosTuristicos/Program.cs
+++ backend/PontosTuristicos/PontosTuristicos/Program.cs
@@ -4,15 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var nomeDoProjeto = app.Configuration.GetValue<string>("NomeDoProjeto");
+var dataDeInicio = app.Configuration.GetValue<string>("DataDeInicio");
+
+
+Console.WriteLine($"Nome do Projeto: {nomeDoProjeto}");
+Console.WriteLine($"Data de inicio: {dataDeInicio}");
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 

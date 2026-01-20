c4c9edfe8256f369d72534012f29dde0061128aa
Leonardo Sato <domme@DESKTOP-BDV1AKL>
2026-01-20 13:12:22 -0300

    Adicionando a entidade `PontoTuristico` e `Entity`

---
+++ backend/PontosTuristicos/PontosTuristicos/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace PontosTuristicos.Entities;
+
+public abstract class Entity
+{
+    Guid Id { get; set; }
+    DateTime DataInclusao { get; set; }
+
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+        DataInclusao = DateTime.Now;
+    }
+}
---
+++ backend/PontosTuristicos/PontosTuristicos/Entities/PontoTuristico.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace PontosTuristicos.Entities;
+
+public class PontoTuristico : Entity
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string Localizacao { get; set; }
+    public string Cidade { get; set; }
+    public string Estado { get; set; }
+
+
+    public PontoTuristico(string nome, string descricao, string localizacao, string cidade, string estado)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Localizacao = localizacao;
+        Cidade = cidade;
+        Estado = estado;
+
+        //Regra para evitar mais de 100 caracteres dentro de descricao
+        if (descricao.Length > 100) 
+        {
+            throw new ArgumentException("A descrição não pode conter mais do que 100 caracteres!");
+        }
+
+
+    }
+}

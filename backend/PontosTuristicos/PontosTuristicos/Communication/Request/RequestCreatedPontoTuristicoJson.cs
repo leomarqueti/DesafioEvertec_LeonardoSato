e2b1e3b723c150a45aa73da478958ddb5dc44199
Leonardo Sato <domme@DESKTOP-BDV1AKL>
2026-01-20 15:40:48 -0300

    Adicionando o `Controler` dos endpoints

---
+++ backend/PontosTuristicos/PontosTuristicos/Communication/Request/RequestCreatedPontoTuristicoJson.cs
@@ -0,0 +1,10 @@
+﻿namespace PontosTuristicos.Communication.Request;
+
+public class RequestCreatedPontoTuristicoJson
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public string Localizacao { get; set; } = string.Empty;
+    public string Cidade { get; set; } = string.Empty;
+    public string Estado { get; set; } = string.Empty;
+}
---
+++ backend/PontosTuristicos/PontosTuristicos/Controllers/PontoTuristicoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PontosTuristicos.Communication.Request;
+using PontosTuristicos.Entities;
+using PontosTuristicos.Data;
+
+namespace PontosTuristicos.Controllers;
+
+[Route("api/[controller]")]  
+public class PontoTuristicoController : ControllerBase
+{
+
+    private readonly PontoTuristicoDb _context;
+
+    public PontoTuristicoController(PontoTuristicoDb context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PontoTuristico), StatusCodes.Status201Created)]
+    public IActionResult Created([FromBody] RequestCreatedPontoTuristicoJson request)
+    {
+        var pontoTuristico = new PontoTuristico(
+            request.Nome,
+            request.Descricao,
+            request.Localizacao,
+            request.Cidade,
+            request.Estado
+            );
+
+        _context.Add(pontoTuristico);
+
+        _context.SaveChanges();
+
+        return Created(string.Empty, pontoTuristico);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(PontoTuristico), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)] 
+
+    public IActionResult GetId([FromRoute] Guid id)
+    {
+        var pontoTuristico = _context.PontosTuristicos.FirstOrDefault(x => x.Id == id);
+
+        if (pontoTuristico == null) { 
+            return NotFound();
+        }
+
+        return Ok(pontoTuristico);
+    }
+   
+}

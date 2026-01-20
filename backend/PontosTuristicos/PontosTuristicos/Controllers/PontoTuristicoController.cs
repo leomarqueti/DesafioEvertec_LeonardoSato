846201ae970dda71f833353f085c9095a43de272
Leonardo Sato <domme@DESKTOP-BDV1AKL>
2026-01-20 16:49:38 -0300

    Finalizado os EndPoints

--- backend/PontosTuristicos/PontosTuristicos/Controllers/PontoTuristicoController.cs
+++ backend/PontosTuristicos/PontosTuristicos/Controllers/PontoTuristicoController.cs
@@ -49,5 +49,49 @@ public class PontoTuristicoController : ControllerBase
 
         return Ok(pontoTuristico);
     }
-   
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+
+    public IActionResult GetAll([FromQuery] int pagina = 1, [FromQuery] string? termo = null)
+    {
+        var query = _context.PontosTuristicos.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(termo))
+        {
+            query = query.Where(p => p.Nome.Contains(termo) || p.Localizacao.Contains(termo) || p.Descricao.Contains(termo));
+
+            
+
+        }
+        query = query.OrderByDescending(p => p.DataInclusao);
+
+        /*
+         .Skip -> fica responsavel por pular pagina ja lida , fazer a paginação
+         .Take -> fica responsavel por pegar sempre o proximos 10
+         */
+        var lista = query.Skip((pagina - 1 ) * 10).Take(10).ToList();
+
+        return Ok(lista);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(PontoTuristico), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        var pontoTuristico = _context.PontosTuristicos.FirstOrDefault(x => x.Id == id);
+
+        if (pontoTuristico == null)
+        {
+            return NotFound();
+        }
+
+        _context.Remove(pontoTuristico);
+
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }

eba585efedbd42882f5845e13d3bf50ca4656f51
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2026-01-22 23:25:18 +0000

    Adicionado o sql de `Procedure`

--- sql_desafio/sql_desafio_extra.sql
+++ sql_desafio/sql_desafio_extra.sql
@@ -9,3 +9,49 @@ SELECT
 FROM Cidades;
 
 
+--Procedure CRUD
+
+CREATE PROCEDURE SP_CRUD_CIDADES
+	@Acao int,
+	@IdCidade int = NULL,
+	@Nome VARCHAR(80) = NULL,
+	@UF VARCHAR(30) = NULL
+AS
+BEGIN
+	-- INSERT
+	IF (@Acao = 1)
+	BEGIN
+		INSERT INTO Cidades (Nome,UF) VALUES ( @Nome, @UF); 
+	END
+
+
+	--UPDATE
+	IF (@Acao = 2)
+	BEGIN
+		IF NOT EXISTS (SELECT 1 FROM Cidades WHERE IdCidade = @IdCidade)
+		BEGIN
+			THROW 50002, 'Nao encontrado CIDADE com esse ID!', 1
+		END
+
+		UPDATE Cidades
+		SET
+			Nome = @Nome,
+			UF = @UF
+		WHERE IdCidade = @IdCidade
+	END
+
+	--DELETE
+	IF (@Acao = 3)
+	BEGIN
+		IF NOT EXISTS (SELECT 1 FROM Cidades WHERE IdCidade = @IdCidade)
+		BEGIN
+			THROW 50002, 'Nao encontrado CIDADE com esse ID!', 1;
+		END 
+
+		DELETE 
+			Cidades
+		WHERE IdCidade = @IdCidade
+	END
+END
+
+
